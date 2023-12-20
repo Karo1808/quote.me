981d9b7b5892e8cb0efdeef1dac54d1729f552ba
Karo <karolp2003@gmail.com>
2023-12-20 12:08:08 +0100

    First deployment attempt

--- quote.me/src/App.tsx
+++ quote.me/src/App.tsx
@@ -1,92 +1,129 @@
 import { useEffect, useState } from "react";
-import { FaRegCopy, FaVolumeHigh } from "react-icons/fa6";
-
-const startingQuote =
-  "Quotes are the concise beauty of profound wisdom, capturing the essence of human thought in a few words";
-
-const API_URL = "https://api.quotable.io";
-
-const COLORS = [
-  "#ffc9c9",
-  "#fcc2d7",
-  "#eebefa",
-  "#d0bfff",
-  "#bac8ff",
-  "#d0ebff",
-  "#c5f6fa",
-  "#c3fae8",
-  "#d3f9d8",
-  "#e9fac8",
-  "#fff3bf",
-  "#ffe8cc",
-  "#e9ecef",
-];
-
-const generateColor = () => Math.floor(Math.random() * COLORS.length);
+import { FaVolumeHigh } from "react-icons/fa6";
+import { FaCopy } from "react-icons/fa";
+import {
+  API_URL,
+  COLORS,
+  MAX_QUOTE_LENGTH,
+  MIN_QUOTE_LENGTH,
+  STARTING_QUOTE,
+} from "./constants";
 
 const App = () => {
-  const [quote, setQuote] = useState(startingQuote);
-  const [wasButtonClicked, setWasButtonClicked] = useState(false);
-  const [color, setColor] = useState(COLORS[COLORS.length - 1]);
+  const [quote, setQuote] = useState<string>(STARTING_QUOTE);
+  const [isButtonClicked, setIsButtonClicked] = useState<boolean>(false);
+  const [color, setColor] = useState<string>(COLORS[COLORS.length - 1]);
+  const [isError, setIsError] = useState<boolean>(false);
+  const [isCopied, setIsCopied] = useState<boolean>(false);
 
   useEffect(() => {
-    const generateQuote = async () => {
+    const fetchRandomQuote = async () => {
       try {
-        if (wasButtonClicked) {
-          const request = await fetch(`${API_URL}/quotes/random`);
+        if (isButtonClicked) {
+          const request = await fetch(
+            `${API_URL}/quotes/random?maxLength=${MAX_QUOTE_LENGTH}&minLength=${MIN_QUOTE_LENGTH}`
+          );
+
+          if (!request.ok) throw new Error("Failed to fetch quote");
           const [newQuote] = await request.json();
-          const newColorIndex = generateColor();
+          const indexOfCurrentColor = COLORS.indexOf(color);
+          const newColorIndex =
+            indexOfCurrentColor + 1 === COLORS.length
+              ? 0
+              : indexOfCurrentColor + 1;
           setColor(COLORS[newColorIndex]);
 
+          setIsError(false);
           setQuote(newQuote.content);
-          setWasButtonClicked(false);
+          setIsButtonClicked(false);
         }
       } catch (error: unknown) {
-        if (error instanceof Error)
+        if (error instanceof Error) {
+          setIsError(true);
           throw new Error(`Failed to fetch quote, ${error.message}`);
+        }
       }
     };
-    generateQuote();
-  }, [wasButtonClicked]);
+    fetchRandomQuote();
+  }, [isButtonClicked, color]);
 
-  const handleClick = () => {
-    setWasButtonClicked(true);
+  const handleQuoteClick = () => {
+    setIsButtonClicked(true);
   };
 
   const handleCopy = async () => {
     const copyToClipboard = async () => {
       try {
         await navigator.clipboard.writeText(quote);
-      } catch (err) {
-        console.error("Unable to copy text to clipboard", err);
+        setIsCopied(true);
+
+        setTimeout(() => {
+          setIsCopied(false);
+        }, 2000);
+      } catch (error: unknown) {
+        if (error instanceof Error)
+          throw new Error(`Unable to copy text to clipboard, ${error.message}`);
       }
     };
     copyToClipboard();
   };
 
   const handleSpeak = () => {
+    const synth = window.speechSynthesis;
+    synth.cancel();
     const utterance = new SpeechSynthesisUtterance(quote);
-    window.speechSynthesis.speak(utterance);
+    synth.speak(utterance);
+  };
+
+  const handleTryAgain = () => {
+    setIsButtonClicked(true);
   };
 
   return (
-    <div className="background" style={{ backgroundColor: color }}>
+    <div
+      className="background"
+      style={{ backgroundColor: `${isError ? "#ff6b6b" : color}` }}
+    >
       <div className="main">
         <div className="container">
-          <blockquote className="quote">&quot;{quote}&quot;</blockquote>
-          <div>
-            <button onClick={handleCopy} className="btn-link">
-              <FaRegCopy />
-            </button>
-            <button onClick={handleSpeak} className="btn-link">
-              <FaVolumeHigh />
-            </button>
-            <button onClick={handleClick} className="btn-quote">
-              New quote
-            </button>
-          </div>
+          {isError ? (
+            <div className="error-container">
+              <p className="error-text">Something went wrong</p>
+              <button onClick={handleTryAgain} className="btn-error">
+                Try again
+              </button>
+            </div>
+          ) : (
+            <>
+              <blockquote style={{ color: color }} className="quote">
+                &quot;{quote}&quot;
+              </blockquote>
+              <div className="btn-container">
+                <button onClick={handleCopy} className="btn-round">
+                  <FaCopy color={color} />
+                </button>
+                <button
+                  style={{ backgroundColor: color }}
+                  onClick={handleQuoteClick}
+                  className="btn-quote"
+                >
+                  New quote
+                </button>
+                <button onClick={handleSpeak} className="btn-round">
+                  <FaVolumeHigh color={color} />
+                </button>
+              </div>
+            </>
+          )}
         </div>
       </div>
+
+      <span
+        style={{ color: color }}
+        className={`copy-message ${isCopied ? "visible" : ""}`}
+      >
+        Successfully copied to clipboard
+      </span>
     </div>
   );
 };
